5afc4f47b055848d6634c0f066345b14a4e1ada8
xStephx <bojkovskistefan9@gmail.com>
2024-05-16 18:37:23 +0200

    initial setup

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                PaleViolet: 'hsl(276, 100%, 81%)',
+                ModerateViolet: 'hsl(276, 55%, 52%)',
+                DesaturatedDarkViolet: 'hsl(271, 15%, 43%)',
+                GrayishBlue: 'hsl(206, 6%, 79%)',
+                VeryDarkDesaturatedViolet: 'hsl(271, 36%, 24%)',
+                DarkGrayishViolet: 'hsl(270, 7%, 64%)',
+                LightMagenta: 'hsl(293, 100%, 63%)',
+                LightViolet: 'hsl(264, 100%, 61%)',
+                White: 'hsl(0, 0%, 100%)',
+                LightGrayishViolet: 'hsl(270, 20%, 96%)',
+                VeryDarkDesaturatedViolet: 'hsl(271, 36%, 24%)',
+                VeryLightMagenta: 'hsl(289, 100%, 72%)',
+                
+            },
+        },
+        fontFamily: {
+            Rubik: ['Rubik', 'sans-serif'],
+        }
+    },
+    plugins: [],
+}
